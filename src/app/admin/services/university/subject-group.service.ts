bf8a496da9d41013f94e9785fd8e0379da4e53c8
Tiến Huỳnh Thanh <tien01662892891@gmail.com>
2021-04-11 22:19:48 +0700

    replace url api by baseUrl

--- src/app/admin/services/university/subject-group.service.ts
+++ src/app/admin/services/university/subject-group.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,9 @@ export class SubjectGroupService {
 
   constructor(private _http: HttpClient) { }
 
+  baseUrl = environment.apiUrl;
+
   getAllSubjectGroup(): Observable<any>{
-    return this._http.get('https://localhost:44344/api/v1/subject-group');
+    return this._http.get(this.baseUrl + 'api/v1/subject-group');
   }  
 }
--- src/app/admin/services/university/university.service.ts
+++ src/app/admin/services/university/university.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { UniversityRM } from '../../view-models';
 
 
@@ -11,19 +12,21 @@ export class UniversityService {
 
   constructor(private _http: HttpClient) { }
 
+  baseUrl = environment.apiUrl;
+
   createUniversity(body: any): Observable<any> {
-    return this._http.post<any>('https://localhost:44344/api/v1/university', body);
+    return this._http.post<any>(this.baseUrl + 'api/v1/university', body);
   }
 
   getAllUniversity(): Observable<any>{
-    return this._http.get('https://localhost:44344/api/v1/university');
+    return this._http.get(this.baseUrl + 'api/v1/university');
   }
 
   getUniversityById(id: string): Observable<any>{
-    return this._http.get('https://localhost:44344/api/v1/university/detail/' + `${id}`);
+    return this._http.get(this.baseUrl + 'api/v1/university/detail/' + `${id}`);
   }
 
   updateUniversity(body: any): Observable<any>{
-    return this._http.put<any>('https://localhost:44344/api/v1/university', body);
+    return this._http.put<any>(this.baseUrl + 'api/v1/university', body);
   }
 }
