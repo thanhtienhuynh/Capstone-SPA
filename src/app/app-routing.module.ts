1fb0c60e83df5b1b0c1bee86900e19e4e02313ba
Tiến Huỳnh Thanh <tien01662892891@gmail.com>
2021-04-13 17:05:18 +0700

    activate auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { StepperComponent } from './major-suggestion-stepper/stepper/stepper.component';
-import { UserComponent } from './user/user.component';
 import { AuthGuard } from './_helper/auth.guard';
 
 const appRoutes: Routes = [
@@ -11,7 +8,7 @@ const appRoutes: Routes = [
   // { path: 'stepper', component: StepperComponent},
   // { path: 'user', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
   // { path: 'home', component: HomeComponent},
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule)}  
+  { path: 'admin', canActivate: [AuthGuard] ,loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule)}  
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -24,7 +24,7 @@ export const environment = {
     measurementId: "G-4HT7GTTN56"
   },
   // apiUrl: 'https://localhost:44344/',
-  apiUrl: 'http://13.212.10.23:63000/',
+  apiUrl: 'https://mohs.azurewebsites.net/',
 };
 
 /*
